7db941b5e72ccc89852546d2dbde88fa3f9b6757
Abolfazl Khalili <kabolfazl39@gmail.com>
2024-08-02 21:43:36 +0330

    Update __init__.py

--- etranslate/__init__.py
+++ etranslate/__init__.py
@@ -2,10 +2,10 @@ from .translate import translate
 
 
 __author__      = 'Abolfazl Khalili'
-__copyright__   = 'Copyright 2021, ixtranslate'
+__copyright__   = 'Copyright 2021, etranslate'
 __credits__     = ['animenosekai']
 __license__     = 'MIT'
-__version__     = 'ixtranslate v0.0.0'
+__version__     = 'etranslate v0.0.0'
 __maintainer__  = 'Abolfazl Khalili'
 __email__       = 'kabolfazl39@gmail.com'
 __status__      = 'Stable'
